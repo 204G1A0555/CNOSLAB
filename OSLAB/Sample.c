bb5670d9601f60027ae5b2377647ddb7914f20b5
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-08-30 11:27:06 +0530

    Update Sample.c

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -1,1 +1,6 @@
-
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No: 204G1A0555
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
