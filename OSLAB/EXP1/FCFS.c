a2c49afab716b03322d2a96f12cb34324b377a0f
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-09-06 12:15:34 +0530

    First list of Errors rectified

--- OSLAB/EXP1/FCFS.c
+++ OSLAB/EXP1/FCFS.c
@@ -6,12 +6,12 @@
 */
 // source code
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
+//clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -34,5 +34,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+//getch();
 }
