2e104fa581ffadd19e84d9bf46df927b3a89448c
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-09-13 10:54:16 +0530

    S Mastan Vali created SJF.c

---
+++ OSLAB/EXP1/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
