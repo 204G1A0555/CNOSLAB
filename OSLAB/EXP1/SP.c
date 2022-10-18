32eeefa7ef32d2ab5a0914c65ba5e48cb448b883
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-10-18 12:06:34 +0530

    S Mastan Vali Created SP.c

---
+++ OSLAB/EXP1/SP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: simulating producer-consumer problem using semaphores
+   Date of Creation: 17-10-2022
+   Date of Execution: 17-10-2022
+*/
