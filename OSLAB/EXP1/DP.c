d8fe9c3ce83f1711754836168ea6ed1354559d29
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-10-18 12:11:35 +0530

    S Mastan Vali Created DP.c

---
+++ OSLAB/EXP1/DP.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: stimulating Dining-philosophers problem
+   Date of Creation: 17-10-2022
+   Date of Execution: 17-10-2022
+*/
