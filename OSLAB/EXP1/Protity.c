363f7280060d6ca5bf228206a264e07749950cd3
204G1A0555 <94515158+204G1A0555@users.noreply.github.com>
2022-10-18 11:56:45 +0530

    S Mastan Vali Created Priority.c file

---
+++ OSLAB/EXP1/Protity.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
