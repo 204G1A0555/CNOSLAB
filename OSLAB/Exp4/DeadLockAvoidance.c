216eeb8004d41484a06d9367250c19291822a089
204G1A0555 <204G1A0555@srit.ac.in>
2022-11-08 11:44:20 +0530

    Mastan vali Sanjamala created DeadLockAvoidance.c

---
+++ OSLAB/Exp4/DeadLockAvoidance.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: A Program to stimulate Bankers algorithm Dead Lock Avoidance
+   Date of Creation: 08-11-2022
+   Date of Execution: 08-11-2022
+*/
---
+++ OSLAB/Exp4/DeadlockAvoidance.c
@@ -0,0 +1,6 @@
+
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: Page Replacement algorithm using fifo
+   Date of Creation: 01-11-2022
+   Date of Execution: 01-11-2022
