8bbe36e6889e263f63afa1a15c7be72d02ac25fb
204G1A0555 <204G1A0555@srit.ac.in>
2022-11-26 21:18:50 +0530

    Checking file exists or not

---
+++ OSLAB/Exp5/MVT.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment:program to stimulate MEMORY MANAGEMENT WITH FIXED PARTITIONING TECHNIQUE(MFT)
+   Date of Creation: 26-11-2022
+   Date of Execution: 26-11-2022
+*/
