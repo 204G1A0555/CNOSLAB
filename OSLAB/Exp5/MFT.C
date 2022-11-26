d9485f2cdac752cf772b6a9c8635cac4f344d008
204G1A0555 <204G1A0555@srit.ac.in>
2022-11-26 22:19:45 +0530

    Mastan Vali Sanjamala created MFT.C file

---
+++ OSLAB/Exp5/MFT.C
@@ -0,0 +1,8 @@
+/*Name Of the Candidate: S. Mastan Vali
+
+   Roll No:  204G1A0555
+   Title of the Experiment:program to stimulate MEMORY VARIABLE PARTIONING TYPE (MVT)
+   Date of Creation: 26-11-2022
+   Date of Execution: 26-11-2022
+*/
+//source code
