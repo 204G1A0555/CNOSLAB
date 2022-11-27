ed1fbdd95d03c127203eaef268668aff0c13b442
204G1A0555 <204G1A0555@srit.ac.in>
2022-11-27 12:58:55 +0530

    Script  started with name date and Mastan Vali Sanjamala created Worstfit.c file

---
+++ OSLAB/Exp6/Worstfit.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S. Mastan Vali
+   Roll No:  204G1A0555
+   Title of the Experiment: To stimulate the Worst fit contiguous memory allocation techniques.
+   Date of Creation: 27-11-2022
+   Date of Execution: 27-11-2022
+*/
