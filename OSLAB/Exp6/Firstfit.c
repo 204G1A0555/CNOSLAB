d81d20cab619179af436fc2596e9481d402705c8
204G1A0555 <204G1A0555@srit.ac.in>
2022-11-27 13:56:29 +0530

    Rectified problems ocrd during compilation and running

--- OSLAB/Exp6/Firstfit.c
+++ OSLAB/Exp6/Firstfit.c
@@ -6,19 +6,112 @@
 */
 //source code
 #include<stdio.h>
-//#include<conio.h>
-#define max 25
 void main()
 {
-int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0; 
-static int bf[max],ff[max];
-//clrscr();
-printf("\n\tMemory Management Scheme - Worst Fit");
-printf("\nEnter the number of blocks:");
-scanf("%d",&nb);
-printf("Enter the number of files:");
-scanf("%d",&nf);
-printf("\nEnter the size of the blocks:-\n");
+	int bsize[10], psize[10], bno, pno, flags[10], allocation[10], i, j;
+	for(i = 0; i < 10; i++)
+	{
+		flags[i] = 0;
+		allocation[i] = -1;
+	}
+	printf("Enter no. of blocks: ");
+	scanf("%d", &bno);
+	printf("\nEnter size of each block: ");
+	for(i = 0; i < bno; i++)
+		scanf("%d", &bsize[i]);
+	printf("\nEnter no. of processes: ");
+	scanf("%d", &pno);
+	printf("\nEnter size of each process: ");
+	for(i = 0; i < pno; i++)
+		scanf("%d", &psize[i]);
+	for(i = 0; i < pno; i++)         //allocation as per first fit
+		for(j = 0; j < bno; j++)
+			if(flags[j] == 0 && bsize[j] >= psize[i])
+			{
+				allocation[j] = i;
+				flags[j] = 1;
+				break;
+			}
+	//display allocation details
+	printf("\nBlock no.\tsize\t\tprocess no.\t\tsize");
+	for(i = 0; i < bno; i++)
+	{
+		printf("\n%d\t\t%d\t\t", i+1, bsize[i]);
+		if(flags[i] == 1)
+			printf("%d\t\t\t%d",allocation[i]+1,psize[allocation[i]]);
+		else
+			printf("Not allocated");
+	}
+}
+
+
+// C implementation of First - Fit algorithm
+/*#include<stdio.h>
+
+// Function to allocate memory to
+// blocks as per First fit algorithm
+void firstFit(int blockSize[], int m, int processSize[], int n)
+{
+	int i, j;
+	// Stores block id of the
+	// block allocated to a process
+	int allocation[n];
+
+	// Initially no block is assigned to any process
+	for(i = 0; i < n; i++)
+	{
+		allocation[i] = -1;
+	}
+
+	// pick each process and find suitable blocks
+	// according to its size ad assign to it
+	for (i = 0; i < n; i++)	 //here, n -> number of processes
+	{
+		for (j = 0; j < m; j++)	 //here, m -> number of blocks
+		{
+			if (blockSize[j] >= processSize[i])
+			{
+				// allocating block j to the ith process
+				allocation[i] = j;
+
+				// Reduce available memory in this block.
+				blockSize[j] -= processSize[i];
+
+				break; //go to the next process in the queue
+			}
+		}
+	}
+
+	printf("\nProcess No.\tProcess Size\tBlock no.\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %i\t\t\t", i+1);
+		printf("%i\t\t\t\t", processSize[i]);
+		if (allocation[i] != -1)
+			printf("%i", allocation[i] + 1);
+		else
+			printf("Not Allocated");
+		printf("\n");
+	}
+}
+
+// Driver code
+int main()
+{
+	int m; //number of blocks in the memory
+	int n; //number of processes in the input queue
+	int blockSize[] = {100, 500, 200, 300, 600};
+	int processSize[] = {212, 417, 112, 426};
+	m = sizeof(blockSize) / sizeof(blockSize[0]);
+	n = sizeof(processSize) / sizeof(processSize[0]);
+
+	firstFit(blockSize, m, processSize, n);
+
+	return 0 ;
+}*/
+
+/*
+:-\n");
 for(i=1;i<=nb;i++)
 {
 printf("Block %d:",i);
@@ -51,4 +144,4 @@ printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
 for(i=1;i<=nf;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 //getch();
-}
+}*/
